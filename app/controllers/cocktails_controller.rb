17308b25897d140dc186c13f0ae4c3f04ede4b9e
adif sgaid <infosgaid@gmail.com>
2020-11-12 20:23:20 +0100

    finito

---
+++ app/controllers/cocktails_controller.rb
@@ -0,0 +1,53 @@
+class CocktailsController < ApplicationController
+  before_action :set_cocktail, only: :show
+  # GET "cocktails"
+  # GET "cocktails" from json
+  def index
+    @cocktails = Cocktail.all
+  end
+
+  # GET /cocktails/1
+  # GET /cocktails/1.json
+  def show
+    @doses = @cocktail.doses
+  end
+  # GET /cocktails/new
+  def new
+    @cocktail = Cocktail.new
+  end
+  # GET /cocktails/1/edit
+
+  # def edit
+  # end
+
+  # POST /cocktails
+  # POST /cocktails.json
+  def create
+    @cocktail = Cocktail.new(cocktail_params)
+    # if @cocktail.save
+    #   redirect_to cocktail_path(@cocktail)
+    # else
+    #   render :new
+    # end
+    respond_to do |format|
+      if @cocktail.save
+        format.html { redirect_to @cocktail, notice: 'Cocktail was successfully created.' }
+        format.json { render :show, status: :created, location: @cocktail }
+      else
+        format.html { render :new }
+        format.json { render json: @cocktail.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_cocktail
+    @cocktail = Cocktail.find(params[:id])
+  end
+
+  def cocktail_params
+    params.require(:cocktail).permit(:name)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'open-uri'
+require 'json'
+puts "Cleaning database..."
+
+Ingredient.destroy_all
+
+puts "Adding ingredients to list..."
+
+url = open('http://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read
+ingredients = JSON.parse(url)
+list = ingredients["drinks"]
+
+list.each do |i|
+  p "Adding #{i['strIngredient1']}"
+  Ingredient.create!(name: i['strIngredient1'])
+end
+
+p "#{Ingredient.count} added!"
