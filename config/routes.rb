fe1630fa576caf130a56a0f48c06464548a06cbb
adif sgaid <infosgaid@gmail.com>
2020-11-13 20:21:52 +0100

    final

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: redirect('/cocktails')
-
-  resources :cocktails, only: [:index, :show, :create, :new] do
-    resources :doses, only: [:create]
+  root 'cocktails#index'
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:new, :create, :destroy]
   end
-
-  resources :doses, only: [:destroy]
 end
